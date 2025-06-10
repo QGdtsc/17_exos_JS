768753df997a87f8febaa2cd00c1dbd23cd57eb2
QGdtsc <qgdtsc@gmail.com>
2025-06-10 16:21:48 +0200

    maj exo5

--- ex1_a_5.js
+++ ex1_a_5.js
@@ -25,15 +25,10 @@
 /*******************************************************************/
 /* EXERCICE 4 */
 
-// let firstName;
-// let lastName;
-// let age; 
-// let emailAddress;
-
-// firstName = prompt("Quel est votre prénom ? ");
-// lastName = prompt("Quel est votre nom ? ");
-// age = prompt("Quel est votre age ? ");
-// emailAddress = prompt("Quel est votre adresse email ? ");
+// let firstName = prompt("Quel est votre prénom ? ");
+// let lastName = prompt("Quel est votre nom ? ");
+// let age = prompt("Quel est votre age ? ");
+// let emailAddress = prompt("Quel est votre adresse email ? ");
 // alert(
 //     "Nom : " + lastName +
 //     "\nPrenom : " + firstName +
@@ -47,13 +42,35 @@
 let nombre1;
 let nombre2;
 let valeurAddition;
+
+
+// Methode 1
 nombre1 = prompt("Entrez un nombre :");
 nombre2 = prompt("Entrez un deuxième nombre :");
 valeurAddition = Number(nombre1) + Number(nombre2)
-alert("L'addition de ces deux nombres donne : " + valeurAddition)
+alert("Methode 1 : Number()\nL'addition de ces deux nombres donne : " + valeurAddition)
+alert( nombre1 + " + " + nombre2 + " = " + (Number(nombre1) + Number(nombre2)))
 
+// Methode 1
+nombre1 = Number(prompt("Entrez un nombre :"));
+nombre2 = Number(prompt("Entrez un deuxième nombre :"));
+valeurAddition = nombre1 + nombre2
+alert("Methode 1 : Number()\nL'addition de ces deux nombres donne : " + valeurAddition)
+alert( nombre1 + " + " + nombre2 + " = " + (nombre1 + nombre2))
 
+// Methode 2
+nombre1 = +prompt("Entrez un nombre :");
+nombre2 = +prompt("Entrez un deuxième nombre :");
+valeurAddition = nombre1 + nombre2
+alert("Methode 2 : +unaire\nL'addition de ces deux nombres donne : " + valeurAddition)
+// alert( nombre1 + " + " + nombre2 + " = " + (Number(nombre1) + Number(nombre2)))
 
+// Methode 3
+nombre1 = parseInt(prompt("Entrez un nombre :"));
+nombre2 = parseInt(prompt("Entrez un deuxième nombre :"));
+valeurAddition = nombre1 + nombre2
+alert("Methode 3 : parseInt\nL'addition de ces deux nombres donne : " + valeurAddition)
+alert(nombre1 + " + " + nombre2 + " = " + (nombre1 + nombre2))
 
 
 
