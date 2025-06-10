79fd27976db6c48b8b174df92567d3e01ef018b0
QGdtsc <qgdtsc@gmail.com>
2025-06-10 14:03:48 +0200

    exercices 1 a 5

---
+++ ex1_a_5.js
@@ -0,0 +1,75 @@
+/*******************************************************************/
+/* EXERCICE 1 */
+
+// let message;
+// message = "hello";
+// alert(message);
+
+/*******************************************************************/
+/* EXERCICE 2 */
+
+// let partOne;
+// let partTwo;
+// partOne="Hello";
+// partTwo="World";
+// alert(partOne + " " + partTwo);
+
+/*******************************************************************/
+/* EXERCICE 3 */
+
+// let firstname;
+// let age;
+// let city;
+// firstname = "Quentin";
+// age = 32;
+// city = "Caen"
+// alert("Je m'appelle " + firstname + ", j'ai " + age + " et j'habite à " + city + ".");
+// alert(`Je m'appelle ${firstname}, j'ai ${age} et j'habite à ${city}`); // utilisation de backstick pour pas que ça pose probleme avec mes apostrophe dans la string.
+
+/*******************************************************************/
+/* EXERCICE 4 */
+
+// let firstName;
+// let lastName;
+// let age; 
+// let emailAddress;
+
+// firstName = prompt("Quel est votre prénom ? ");
+// lastName = prompt("Quel est votre nom ? ");
+// age = prompt("Quel est votre age ? ");
+// emailAddress = prompt("Quel est votre adresse email ? ");
+// alert(
+//     "Nom : " + lastName +
+//     "\nPrenom : " + firstName +
+//     "\nAge : " + age +
+//     " ans \nAdresse email : " + emailAddress
+// );
+
+/*******************************************************************/
+/* EXERCICE 5 */
+
+let nombre1;
+let nombre2;
+let valeurAddition;
+nombre1 = prompt("Entrez un nombre :");
+nombre2 = prompt("Entrez un deuxième nombre :");
+valeurAddition = Number(nombre1) + Number(nombre2)
+alert("L'addition de ces deux nombres donne : " + valeurAddition)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
