b7a1e05e545ad495900227a430e12cb700e8d510
QGdtsc <qgdtsc@gmail.com>
2025-06-16 14:16:12 +0200

    maj exos tableaux

--- exo_1_a_6_tableaux.js
+++ exo_1_a_6_tableaux.js
@@ -16,8 +16,10 @@
 //     "Decembre",
 // ]
 // for (i = 0; i < 12; i++) {
-//     console.log(month[i])
+//     console.log(listMonth[i])
 // }
+// console.log(listMonth)
+// console.table(listMonth)
 
 /*******************************************************************/
 /* EXERCICE 2 */
@@ -32,7 +34,6 @@
 //     "Dimanche"
 // ]
 // console.log(listDay[4])
-
 
 /*******************************************************************/
 /* EXERCICE 3*/
@@ -65,9 +66,30 @@
 //     "Novembre",
 //     "Decembre",
 // ]
-// for (i = 0; i < 12; i++) {
+// // avec boucle for
+// for ( let i = 0; i < 12; i++) {
 //     console.log(i + 1 + " - " + listMonth[i]) // utilisation de i+1 pour le numero a afficher car le 1er element de la liste est 0
 // }
+// // avec boucle foreach
+// // let i = 1
+// listMonth.forEach((value, index) => {
+//     index++
+//     console.log(index + " - " + value)
+//     // i++
+// });
+
+
+// console.log("\n\n\n\n")
+// // ou
+// listMonth.forEach((value, index) => {
+//     console.log((index + 1) + " - " + value)
+// });
+
+// console.log("\n\n\n\n")
+// // ou
+// listMonth.forEach(value => {
+//     console.log((listMonth.indexOf(value) + 1) + " - " + value)
+// });
 
 
 /*******************************************************************/
@@ -119,6 +141,8 @@
 //     "Saturday",
 //     "Sunday"
 // ]
+
+
 // let aggregatedArray = [
 //     frenchMonth,
 //     frenchDay,
@@ -150,6 +174,28 @@
 // }
 // console.log(str_result)
 
+
+// // Correction
+// console.log("\n\n\nCORRECTION\n\n")
+// let tableau_french = [frenchMonth, frenchDay]
+// let tableau_english = [englishMonth, englishDay]
+// let tableau_total = [tableau_french, tableau_english]
+// // ou tableau total = [frenchMonth, frenchDay], [englishMonth, englishDay]
+// // Afficher Novembre 
+// console.log(tableau_total[0][0][10])
+// // Afficher Friday 
+// console.log(tableau_total[1][1][4])
+// // Afficher tous les mois en anglais
+// console.log(tableau_total[1][0])
+// for (i = 0; i < tableau_total[1][0].length; i++) {
+//     console.log(tableau_total[1][0][i])
+// }
+// // Afficher tous les jours en français
+// console.log(tableau_total[0][1])
+// for (i = 0; i < tableau_total[0][1].length; i++) {
+//     console.log(tableau_total[0][1][i])
+// }
+
 /*******************************************************************/
 /* EXERCICE 6 */
 
@@ -159,6 +205,7 @@ console.log(listPrincesses)
 // Ajouter des elements avec push
 listPrincesses.push("Mulan", "Peach", "Raiponce", "Belle", "Vaiana", "Daisy", "Jasmine")
 console.log(listPrincesses)
+
 // Supprimer 2 elements
 // Methode 1
 // Suppression de Peach
@@ -167,16 +214,40 @@ let indexElementToBeRemoved = listPrincesses.indexOf("Peach")
 // console.log(indexElementToBeRemoved +" is the index of element Peach in the list.")
 // Supprimer l'element grace a l'index
 listPrincesses.splice(indexElementToBeRemoved,1)
+// ou 
+// listPrincesses.splice(listPrincesses.indexOf("Peach"),1)
 // Suppression de Daisy
-indexElementToBeRemoved = listPrincesses.indexOf("Daisy")
-listPrincesses.splice(indexElementToBeRemoved,1)
+// indexElementToBeRemoved = listPrincesses.indexOf("Daisy")
+// listPrincesses.splice(indexElementToBeRemoved,1)
+// ou
+let filteredList = listPrincesses.filter(names => names != "Daisy" && names != "Vaiana")
+console.log(filteredList)
+
 // Afficher la list apres les 2 suppressions
 console.log(listPrincesses)
 // Trier le tableau alphabetiquement
 listPrincesses.sort()
 console.log(listPrincesses)
+
+// Trier le tableau alphabetiquement a l'envers
+listPrincesses.sort().reverse()
+console.log(listPrincesses)
+
 // Afficher le nombre de princesses dans le tableau
 listPrincesses.length > 1 ? console.log(`Il y a ${listPrincesses.length} princesses dans le tableau`) : console.log(`Il y a ${listPrincesses.length} princesse dans le tableau`)
 
+// BONUS : Utilisation de shift et unshift
+console.log("\n\n\n")
+// shift pour enlever le 1er element de l'array
+listPrincesses.shift()
+console.log(listPrincesses)
+// unshift ajout un element au début
+listPrincesses.unshift("blanche Neige")
+console.log(listPrincesses)
 
+// BONUS : Afficher les elements les uns a la suite des autres avec une virgule entre chaque element
+// remettre par orfre alphabetique
+listPrincesses.sort()
+listPrincesses = listPrincesses.join(" , ")
+console.log(listPrincesses)
 
